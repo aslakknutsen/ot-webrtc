0f7d9b37f1ade746fc5d364a36d8cb1cb2fab009
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-01-15 20:20:39 +0100

    Handling updates for code-mirror

--- client/app/scripts/init.js
+++ client/app/scripts/init.js
@@ -8,13 +8,86 @@ $(document).ready(function() {
 
     var cmAdapter = new ot.CodeMirrorAdapter(cm);
 
-    cmAdapter.registerCallbacks({
-        change: function (operation, inversion) {
-            console.log('operation', operation);
-            console.log('inversion', inversion);
-            // TODO pass message to the peer
-        }
+
+    var user = getParameterByName("user")
+
+    var peer = new Peer(user, { host: '192.168.0.174', port: 9000, secure: false, debug: 3 });
+    peer.on('open', function (id) {
+        console.log('My peer ID is: ' + id);
+
     });
+    peer.on('connection', function (conn) {
+        cmAdapter.registerCallbacks({
+            change: function (operation, _) {
+                conn.send(operation);
+            }
+        });
+        conn.on('data', function (data) {
+            console.log('Received', data);
+            cmAdapter.applyOperation(data);
+            // Update cm
+        });
+    });
+
+
+    var session = Session('http://192.168.0.174:8080', 'A')
+    $('#share').click(function () {
+        session.whoami = function () {
+            return {
+                'name': user,
+            }
+        }
+        session.start()
+    })
+
+    setInterval(function () {
+        session.sessions().success(function (resp) {
+            $('#sessions').html('')
+            for (var i = 0; i < resp.length; i++) {
+                var s = $('<div>')
+                s.append($('<button>join</button>').click(
+                    function (index) {
+                        return function () {
+                            session.whoami = function () {
+                                return {
+                                    name: user
+                                }
+                            };
+                            session.join(index)
+                            var conn = peer.connect(resp[index].owner.name);
+                            conn.on('open', function () {
+                                cmAdapter.registerCallbacks({
+                                    change: function (operation, _) {
+                                        conn.send(operation);
+                                    }
+                                });
+                                conn.on('data', function (data) {
+                                    console.log('Received', data);
+                                    cmAdapter.applyOperation(data);
+                                });
+                            });
+                        }
+                    }(i)));
+                s.append($("<div>" + JSON.stringify(resp[i], null, 4) + "</div>"))
+                $("#sessions").append(s).append($("<br/>"))
+            }
+        })
+    }, 2000);
+
+
+
+
+    function getParameterByName(name, url) {
+        if (!url) {
+            url = window.location.href;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 
     // TODO listen to messages
 
