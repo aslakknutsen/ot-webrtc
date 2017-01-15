187c80e33767c1aa454f0bbe98f8ab3b1160620e
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-01-15 18:46:59 +0100

    Stubs codemirror-ot handler

--- client/app/scripts/init.js
+++ client/app/scripts/init.js
@@ -2,8 +2,20 @@
 
 $(document).ready(function() {
 
-    CodeMirror.fromTextArea(document.getElementById('editor'), {
+    var cm = CodeMirror.fromTextArea(document.getElementById('editor'), {
         lineNumbers: true
     });
+
+    var cmAdapter = new ot.CodeMirrorAdapter(cm);
+
+    cmAdapter.registerCallbacks({
+        change: function (operation, inversion) {
+            console.log('operation', operation);
+            console.log('inversion', inversion);
+            // TODO pass message to the peer
+        }
+    });
+
+    // TODO listen to messages
 
 });
